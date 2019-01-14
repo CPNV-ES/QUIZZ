d66bb9dfa9a7b6447621751541f05fb77e0062ac
Steven Avelino <stevenavelino@Stevens-MacBook-Pro.local>
2019-01-14 11:42:28 +0100

    Add a condition to check_token and a check_guest method

--- utils/security.py
+++ utils/security.py
@@ -7,19 +7,33 @@ Function to check if the token sent is valid
 
 Returns a boolean
 '''
-def check_token(token, require_admin):
+def check_token(token, require_admin, require_creator):
     decrypted_token = decrypt(token)
     user = User.objects.get(username=decrypted_token['username'])
     if (user):
         if (decrypted_token['password'] == user.password):
             if (require_admin):
                 return True if user.admin == True else False
+            elif (require_creator):
+                return True if user.creator == True or user.admin == True else False
             else:
                 return True
         else:
             return False
     else:
         return False
+
+'''
+Function that checks if an user is a guest
+'''
+def check_guest(token):
+    decrypted_token = decrypt(token)
+    user = User.objects.get(username=decrypted_token['username'])
+    if (user):
+        if (user.guest):
+            return True
+        else:
+            return False
 
 '''
 Context object for password encryptions
