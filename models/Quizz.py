45ebbf0cd33d8df2ffae4853784a2448049ced2f
Steven Avelino <stevenavelino@Stevens-MacBook-Pro.local>
2019-01-14 10:50:07 +0100

    Only display quizz attributes instead of everything + questions

--- models/Quizz.py
+++ models/Quizz.py
@@ -8,8 +8,8 @@ class CustomQuerySet(QuerySet):
         return "[%s]" % (",".join([doc.to_json() for doc in self]))
 
 class Quizz(Document):
-    name = StringField(required=True, max_length=255)
-    image = ImageField()
+    title = StringField(required=True, max_length=255)
+    image = StringField(required=True)
     description = StringField(required=True)
     questions = ListField(ReferenceField(Question))
     created_by = ReferenceField(User)
@@ -19,11 +19,8 @@ class Quizz(Document):
 
     def to_json(self):
         data = self.to_mongo()
-        questions = []
-        for question in self.questions:
-            questions.append({ "name": question.name, "answers": question.answers })
-        data['questions'] = questions
         data.pop('_id')
+        data.pop('questions')
         data['id'] = str(self.pk)
         data['created_by'] = {"id": str(self.created_by.pk), "username": self.created_by.username}
         return json_util.dumps(data)
