253fafcb59b38f3f3f8906732114af4741712626
Steven Avelino <stevenavelino@Stevens-MacBook-Pro.local>
2019-01-18 17:13:27 +0100

    Override to_json method in User model

--- models/User.py
+++ models/User.py
@@ -5,6 +5,16 @@ Author : Steven Avelino
 '''
 
 from mongoengine import *
+from bson import json_util
+
+'''
+Custom queryset for the class
+
+Created for the purpose to modify the to_json method
+'''
+class CustomQuerySet(QuerySet):
+    def to_json(self):
+        return "[%s]" % (",".join([doc.to_json() for doc in self]))
 
 '''
 Main class for the model User
@@ -17,3 +27,24 @@ class User(Document):
     admin = BooleanField(required=True, default=False)
     creator = BooleanField(required=True, default=False)
     guest = BooleanField(required=True, default=False)
+
+    # Change the queryset class to the custom one
+    meta = {'queryset_class': CustomQuerySet}
+
+    # Override to_json method
+    def to_json(self):
+         # Get the object to a mongo object
+        data = self.to_mongo()
+        # Remove the default _id key from the dict as it is badly formatted
+        data.pop('_id')
+        # Remove the password from the json
+        data.pop('password')
+        # Remove the token from the json
+        data.pop('token')
+        if len(data['scores']) > 0:
+            for index, score in enumerate(data['scores']):
+                data['scores'][index]['quizz_id'] = str(score['quizz_id'])
+        # Add a correctly formatted id key in the dict
+        data['id'] = str(self.pk)
+        # Return the dict as JSON
+        return json_util.dumps(data)
