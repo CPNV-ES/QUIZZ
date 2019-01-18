f1110a073f8f416c0432f7bc72a6b70f9da883bd
Steven Avelino <stevenavelino@Stevens-MacBook-Pro.local>
2019-01-18 16:26:23 +0100

    Fix check of user document in security.py

--- utils/security.py
+++ utils/security.py
@@ -14,8 +14,8 @@ Returns a boolean
 '''
 def check_token(token, require_admin, require_creator):
     decrypted_token = decrypt(token)
-    user = User.objects.get(username=decrypted_token['username'])
-    if (user):
+    if (User.objects(username=decrypted_token['username'])):
+        user = User.objects.get(username=decrypted_token['username'])
         if (decrypted_token['password'] == user.password):
             if (require_admin):
                 return True if user.admin == True else False
@@ -33,12 +33,14 @@ Function that checks if an user is a guest
 '''
 def check_guest(token):
     decrypted_token = decrypt(token)
-    user = User.objects.get(username=decrypted_token['username'])
-    if (user):
+    if (User.objects(username=decrypted_token['username'])):
+        user = User.objects.get(username=decrypted_token['username'])
         if (user.guest):
             return True
         else:
             return False
+    else:
+        return False
 
 '''
 Context object for password encryptions
